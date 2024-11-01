b18f1fdec9b8a589a8ffc645a5e57925aca60ac6
ZachVFX <zachallrd@gmail.com>
2024-11-01 13:07:46 +0100

    Ui tweak

--- ui/ctk_dialog.py
+++ ui/ctk_dialog.py
@@ -5,9 +5,10 @@ class TaskDialog(ctk.CTkToplevel):
     def __init__(self, parent, title: str, description: str, button_text: str):
         super().__init__(parent)
         self.title(title)
-        self.geometry("300x300")
+        self.geometry("300x150")
         self.task_title = None
         self.task_content = None
+        self.grid_columnconfigure(0, weight=1)
 
         self.label = ctk.CTkLabel(self, text=description)
         self.label.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
@@ -16,7 +17,7 @@ class TaskDialog(ctk.CTkToplevel):
         self.entry.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
 
         self.button = ctk.CTkButton(self, text=button_text, command=self.on_add)
-        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="nsew")
+        self.button.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
 
         self.entry.bind("<Return>", self.on_add)
 
